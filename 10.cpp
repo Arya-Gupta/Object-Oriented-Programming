b705f50665bf899e4ee602d8a0705e717324d53a
Arya Gupta <arya.gupta99999@gmail.com>
2022-07-28 19:38:13 +0530

    Parameterized and Default Constructors

---
+++ 10.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+    public:
+        Complex(int, int); // Constructor declaration
+
+        void printNumber(){
+            cout << "Your number is " << a << " + " << b << "i" << endl;
+        }
+};
+
+// Parameterized constructor as it takes 2 parameters
+Complex ::Complex(int x, int y){
+    a = x;
+    b = y;
+}
+
+int main(){
+    // Implicit call
+    Complex a(4, 6);
+    a.printNumber();
+
+    // Explicit call
+    Complex b = Complex(5, 7);
+    b.printNumber();
+
+    return 0;
+}
